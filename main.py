399e1c23d13ad4028baa5b0ca5dc6416dc72b61f
Vallerio <vrayford@student.ciputra.ac.id>
2024-11-25 18:00:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from Fitur.division import calculate_division
-
-a = 10
-b = 2
-
-division_result = calculate_division(a, b)
-print(f"division result is: {division_result}")
-
 from Fitur.sum import calculate_sum
 
 A = int(input("Masukkan A: "))
 B = int(input("Masukkan B: "))
 
-sumResult = calculate_sum(A, B)
-print(f"Sum: {sumResult}")
+print("1. Sum\n4. Division")
+choice = int(input("Which operation do you want to do?")
+
+
+if choice == 1:
+  print(f"Sum : {calculate_sum(A,B)}")
+division_result = calculate_division(A, B)
+print(f"division result is: {division_result}")
+
